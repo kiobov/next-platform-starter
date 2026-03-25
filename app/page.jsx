c348a011225d5aa09d44fe63b50ee280a93b36da
nyakio <nyakio19roseary@gmail.com>
2026-03-25 04:26:43 +0300

    file  naming update

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,23 @@
 import Link from 'next/link';
-import {Header} from 'components/Header'
+import {Header} from 'components/header'
 import {Features} from 'components/Features'
 import {Projects} from 'components/Projects'
 import {Testimonials} from 'components/Testimonials'
 import {Technologies} from 'components/Technologies'
 import {CaseStudy} from 'components/CaseStudy'
-import {Footer} from 'components/Footer'
+import {Footer} from 'components/footer'
 
 
 
 export default function Page() {
 	return (<div style = {{backgroundColor: 'white'}}>
-		<Header />
-		<Features />
-		<Projects />
-		<Testimonials />
+		<Header/>
+		<Features/>
+		<Projects/>
+		<Testimonials/>
 		<Technologies/>
-		<CaseStudy />
-		<Footer />
+		<CaseStudy/>
+		<Footer/>
 
 
 		</div>);

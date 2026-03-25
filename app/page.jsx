ffb4f24381140e7a6772265c9e6b0e347b2b3565
nyakio <nyakio19roseary@gmail.com>
2026-03-25 04:22:02 +0300

    feature error resolving(typo)

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import {Header} from 'components/Header'
-import {Features} from 'components/Feature'
+import {Features} from 'components/Features'
 import {Projects} from 'components/Projects'
 import {Testimonials} from 'components/Testimonials'
 import {Technologies} from 'components/Technologies'

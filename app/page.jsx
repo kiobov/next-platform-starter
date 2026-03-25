95628a017ee7f3c01b7e4f48303d098e14d61b55
nyakio <nyakio19roseary@gmail.com>
2026-03-25 04:17:34 +0300

    main page background color change

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,24 @@
 import Link from 'next/link';
+import {Header} from 'components/Header'
+import {Features} from 'components/Feature'
+import {Projects} from 'components/Projects'
+import {Testimonials} from 'components/Testimonials'
+import {Technologies} from 'components/Technologies'
+import {CaseStudy} from 'components/CaseStudy'
+import {Footer} from 'components/Footer'
+
 
 
 export default function Page() {
-	return (<div>
+	return (<div style = {{backgroundColor: 'white'}}>
+		<Header />
+		<Features />
+		<Projects />
+		<Testimonials />
+		<Technologies/>
+		<CaseStudy />
+		<Footer />
+
 
 		</div>);
 
